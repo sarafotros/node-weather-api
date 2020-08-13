48e70765b21af984b77bd6ed11e1ecdd6b31162e
Sara Fotros <sarafotros@gmail.com>
2020-08-13 12:32:05 +0100

    error hadling

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const hbs = require('hbs');
+// let ejs = require('ejs');
 const path = require('path');
 const axios = require('axios');
 const app = express();
 
-const apiKey = 'dd956bf8419d0456adb3ff56d7ad1041';
+
 
 const publicDirectory = path.join(__dirname, '/public');
 
@@ -14,27 +14,61 @@ const viewsPath = path.join(__dirname + '/views');
 
 app.set('view engine', 'hbs');
 
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  const apiKey = 'dd956bf8419d0456adb3ff56d7ad1041';
-  const cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${apiKey}`;
+//   const apiKey = 'dd956bf8419d0456adb3ff56d7ad1041';
+//   const cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=london,uk&appid=${apiKey}`;
 
-  const apiResponse = await axios.get(cityUrl);
+//   const apiResponse = await axios.get(cityUrl);
 
-  let iconUrl = "http://openweathermap.org/img/w/" + apiResponse.data.weather[0].icon + ".png";
-  let tempC = (apiResponse.data.main.temp - 273.15).toFixed(2)
+//   let iconUrl = "http://openweathermap.org/img/w/" + apiResponse.data.weather[0].icon + ".png";
+//   let tempC = (apiResponse.data.main.temp - 273.15).toFixed(2)
 
-
- res.render('index', {
-    weatherDisc: apiResponse.data.weather[0].description,
-    tempC:  tempC,
-    srcLogo: iconUrl,
+res.render('index', {
+    weatherDisc: '',
+    tempC:  '',
+    srcLogo: "http://openweathermap.org/img/w/04d.png"
   });
 });
 
+app.post('/', async (req, res) => {
+    try{
+    const city = req.body.theCity
+    const countCode = req.body.theCountry
+    const apiKey = 'dd956bf8419d0456adb3ff56d7ad1041';
+    const cityUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countCode}&appid=${apiKey}`;
+    const apiResponse = await axios.get(cityUrl);
+    let iconUrl = "http://openweathermap.org/img/w/" + apiResponse.data.weather[0].icon + ".png";
+    let tempC = (apiResponse.data.main.temp - 273.15).toFixed(2)
+
+
+    
+  
+   res.render('index', {
+       cityName: city,
+      weatherDisc: apiResponse.data.weather[0].description,
+      tempC:  tempC,
+      srcLogo: iconUrl,
+    });}
+    catch (error) {
+        console.error(error.response);
+        res.render('index',{
+            msg: error.response.data.message,
+            srcLogo: '/images/e1.png'
+        });
+      }
+})
+
+
+// 7 days forecast
 app.get('/weather', (req, res) => {
+
+
+
    res.render('weather') 
 }
 )
