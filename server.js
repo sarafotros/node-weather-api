b568ad76f416b96b89b0818b3f1cb311c4ed894a
Sara Fotros <sarafotros@gmail.com>
2020-08-14 15:35:31 +0100

    netlify depoy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const axios = require('axios');
 const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const publicDirectory = path.join(__dirname, '/public');
 
